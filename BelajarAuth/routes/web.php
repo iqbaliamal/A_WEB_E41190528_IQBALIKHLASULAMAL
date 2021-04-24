e6bb1b1ea32fd1b42d2aa1c4a89786d28fe1020b
iqbaliamal <iqbaliamal@gmail.com>
2021-04-24 20:58:53 +0700

    add tugas auth

---
+++ BelajarAuth/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Middleware\Role;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('role');
+Route::get('error', function () {
+    return view('error');
+})->name('error');
